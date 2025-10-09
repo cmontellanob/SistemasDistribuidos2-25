2d89269e057abea60296b8b9a30f2f61f3816502
cmontellanob <carlosmontellano@gmail.com>
2025-10-09 17:37:47 -0400

    09-10-25

---
+++ 09-10 Practica SOAP/WebSite1/WebSite1/App_Code/Cotizacion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Clima
+/// </summary>
+public class Cotizacion
+{
+    public DateTime fecha;
+    public decimal cotizacion;
+    public decimal cotizacionOficial;
+    public Cotizacion(DateTime fecha, decimal cotizacion, decimal cotizacionOficial)
+    {
+        this.fecha = fecha;
+        this.cotizacion = cotizacion;
+        this.cotizacionOficial = cotizacionOficial;
+    }
+    public Cotizacion()
+    {
+
+         
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+
+}
---
+++ 09-10 Practica SOAP/WebSite1/WebSite1/App_Code/CotizacionDAO.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Activities.Statements;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+/// <summary>
+/// Descripción breve de CotizacionDAO
+/// </summary>
+public class CotizacionDAO
+{
+    private MySqlConnection conexion;
+    
+    public CotizacionDAO(string connectionString = "Server=localhost;Database=bd_Cotizacion;User ID=root;Password=;SslMode=none;")
+
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+        conexion = new MySqlConnection(connectionString);
+    }
+    public List<Cotizacion> obtenerCotizacion()
+
+    { 
+        List<Cotizacion> lista = new List<Cotizacion>();
+        conexion.Open();
+
+        string query = "SELECT fecha, cotizacion,cotizacion_oficial FROM Cotizacions";
+        MySqlCommand comando = new MySqlCommand(query, conexion);
+        MySqlDataReader reader = comando.ExecuteReader();
+
+        while (reader.Read())
+        {
+            DateTime fecha = reader.GetDateTime("fecha");
+            decimal cotizacion = reader.GetDecimal("cotizacion");
+            decimal cotizacionOficial = reader.GetDecimal("cotizacion_oficial");
+            lista.Add(new Cotizacion(fecha, cotizacion, cotizacionOficial));
+
+        }
+        conexion.Close();
+        return lista;
+
+    }
+    public bool agregarCotizacion(Cotizacion cotizacion)
+    {
+        try
+        conexion.Open();
+        string query = "INSERT INTO Cotizacions (fecha, cotizacion, cotizacion_oficial) VALUES (@fecha, @cotizacion, @cotizacion_oficial)";
+        MySqlCommand comando = new MySqlCommand(query, conexion);
+        comando.Parameters.AddWithValue("@fecha", cotizacion.fecha);
+        comando.Parameters.AddWithValue("@cotizacion", cotizacion.cotizacion);
+        comando.Parameters.AddWithValue("@cotizacion_oficial", cotizacion.cotizacionOficial);
+        comando.ExecuteNonQuery();
+        conexion.Close();
+        return true;
+        Catch
+        {
+            return false;
+        }
+        }
+    public void eliminarCotizacion(DateTime fecha)
+    {
+        conexion.Open();
+        string query = "DELETE FROM Cotizacions WHERE fecha = @fecha";
+        MySqlCommand comando = new MySqlCommand(query, conexion);
+        comando.Parameters.AddWithValue("@fecha", fecha);
+        comando.ExecuteNonQuery();
+        conexion.Close();
+    }
+
+    public void actualizarCotizacion(Cotizacion Cotizacion)
+    {
+        conexion.Open();
+        string query = "UPDATE Cotizacions SET cotizacion = @cotizacion, cotizacion_oficial = @cotizacion_oficial WHERE fecha = @fecha";
+        MySqlCommand comando = new MySqlCommand(query, conexion);
+        comando.Parameters.AddWithValue("@fecha", Cotizacion.fecha);
+        comando.Parameters.AddWithValue("@cotizacion", Cotizacion.cotizacion);
+        comando.Parameters.AddWithValue("@cotizacion_oficial", Cotizacion.cotizacionOficial);
+        comando.ExecuteNonQuery();
+        conexion.Close();
+    }
+
+    public Cotizacion obtenerCotizacionPorFecha(DateTime fecha)
+    {
+        conexion.Open();
+        string query = "SELECT fecha, cotizacion, cotizacion_oficial FROM Cotizacions WHERE fecha = @fecha";
+        MySqlCommand comando = new MySqlCommand(query, conexion);
+        comando.Parameters.AddWithValue("@fecha", fecha);
+        MySqlDataReader reader = comando.ExecuteReader();
+        Cotizacion Cotizacion = null;
+        if (reader.Read())
+        {
+            DateTime fechaObtenida = reader.GetDateTime("fecha");
+            decimal cotizacion = reader.GetDecimal("cotizacion");
+            decimal cotizacionOficial = reader.GetDecimal("cotizacion_oficial");
+            Cotizacion = new Cotizacion(fechaObtenida, cotizacion, cotizacionOficial);
+        }
+        conexion.Close();
+        return Cotizacion;
+    }
+
+}
